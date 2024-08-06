2316fb2555542b13d47cace62702736e38d4ed4c
rac640 <50179707+rac640@users.noreply.github.com>
2024-08-05 20:53:58 -0400

    Completed getHumanChoice function to get human choice

--- javascript.js
+++ javascript.js
@@ -20,4 +20,31 @@ function getComputerChoice (){
 
 getComputerChoice()
 
+// step 3: logic for getting human choice 
+function getHumanChoice(){
+    humanInput = prompt("rock, paper, or scissors?");
 
+    // This trims and lowercases the user's input 
+    finalHumanInput = humanInput.trim().toLowerCase();
+
+    if (finalHumanInput ==="rock"){
+        console.log("You: rock");
+        return "rock";
+    }
+    else if (finalHumanInput ==="paper"){
+        console.log("You: paper");
+        return "paper";
+    }
+    else if (finalHumanInput ==="scissors"){
+        console.log("You: scissors");
+        return "scissors";
+    }
+    // If the user inputs anything other than rock paper or scissors, it goes back to the prompt (Credit: https://stackoverflow.com/questions/40606697/how-do-i-loop-back-to-the-prompt-if-answer-is-incorrect)
+    else{
+        getHumanChoice();
+    }
+
+}
+
+
+getHumanChoice()
